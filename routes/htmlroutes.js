4368bc4c3951fd68da71e36124ba50031d187ac9
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2021-08-19 01:36:32 -0400

    added comments

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const path = require('path');
+//imports
+
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+//GET route for notes
 
 //default route = index.html from public
 router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+//GET route for the main page with *
 
 module.exports = router;
