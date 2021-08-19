93342a2ae362ca7e78852258db051feeca3b32c3
Zachary Zaleski <83981829+kingcrowz@users.noreply.github.com>
2021-08-19 01:35:29 -0400

    added comments

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const fs = require('fs');
 const htmlroutes = require('./routes/htmlroutes');
 const apiroutes = require("./routes/apiroutes");
-
+//imports
 
 const PORT = process.env.PORT || 3001
-
 const app = express();
 
 app.use(express.json());
@@ -13,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(apiroutes);
 app.use(htmlroutes);
+//custom middleware
 
 app.listen(PORT, () =>
   console.log(`Reference: http://localhost:${PORT}`)
